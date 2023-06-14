ee729688669b8a6e8d8da9df415d43cef422302c
vsantsal <vsantsal@gmail.com>
2023-06-13 21:45:34 -0300

    diretorio de projeto spring para backend

---
+++ backend/src/main/java/br/pucrioaps/livrarialdw/LivrariaLdwApplication.java
@@ -0,0 +1,13 @@
+package br.pucrioaps.livrarialdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaLdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaLdwApplication.class, args);
+	}
+
+}
