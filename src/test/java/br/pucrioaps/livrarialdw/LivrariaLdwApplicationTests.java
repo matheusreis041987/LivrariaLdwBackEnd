69ac090d3b30ab4cca5b2de307e19e9263271815
vsantsal <vsantsal@gmail.com>
2023-06-16 21:01:27 -0300

    estrutura basica de projeto spring boot para o backend

---
+++ src/test/java/br/pucrioaps/livrarialdw/LivrariaLdwApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrioaps.livrarialdw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaLdwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
