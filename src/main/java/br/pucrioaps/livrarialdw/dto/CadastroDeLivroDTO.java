5325b4d35c05124abf0ec0272521cfd69b6e32dd
vsantsal <vsantsal@gmail.com>
2023-06-22 13:33:10 -0300

    validações para dto de cadastro de livro

--- src/main/java/br/pucrioaps/livrarialdw/dto/CadastroDeLivroDTO.java
+++ src/main/java/br/pucrioaps/livrarialdw/dto/CadastroDeLivroDTO.java
@@ -2,15 +2,22 @@ package br.pucrioaps.livrarialdw.dto;
 
 import br.pucrioaps.livrarialdw.model.entity.Categoria;
 import br.pucrioaps.livrarialdw.model.entity.Livro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record CadastroDeLivroDTO(
         String isbn,
+        @NotBlank(message = "título é obrigatório")
         String titulo,
+        @NotBlank(message = "autoria é obrigatória")
         String autoria,
+        @NotBlank(message = "autoria é obrigatória")
         String editora,
+        @NotBlank(message = "informar categoria válida")
         String categoria,
+        @NotNull(message = "preço de venda obrigatório")
         BigDecimal precoVenda
 ) {
     public Livro toLivro() {
