d60dc090f480fc6b787681f8a29028d0e00bc962
vsantsal <vsantsal@gmail.com>
2023-06-22 16:30:12 -0300

    create table no script compativel com postgresql para develop

--- src/main/resources/db/migration/V1__create-table-livro.sql
+++ src/main/resources/db/migration/V1__create-table-livro.sql
@@ -1,8 +1,8 @@
 create table livro(
     -- chave primária para postgresql
-    -- id serial primary key,
+    id serial primary key,
     -- chave primária para mysql
-    id bigint primary key auto_increment,
+    --    id bigint primary key auto_increment,
     isbn varchar(13) not null unique,
     titulo varchar(200) not null,
     autoria varchar(200) not null,
