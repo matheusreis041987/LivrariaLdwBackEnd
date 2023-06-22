fabf3ed9be7546d882acbc81a2c66e95614d0a0d
vsantsal <vsantsal@gmail.com>
2023-06-21 21:27:44 -0300

    endpoint de detalhes de livro funcional para id valido

---
+++ src/main/java/br/pucrioaps/livrarialdw/dto/DetalheDeLivroDTO.java
@@ -0,0 +1,20 @@
+package br.pucrioaps.livrarialdw.dto;
+
+import br.pucrioaps.livrarialdw.model.Livro;
+
+import java.math.BigDecimal;
+
+public record DetalheDeLivroDTO(
+        Long id,
+        String isbn,
+        String titulo,
+        String autoria,
+        String editora,
+        String categoria,
+        BigDecimal precoVenda) {
+
+    public DetalheDeLivroDTO(Livro livro){
+        this(livro.getId(), livro.getIsbn(), livro.getTitulo(), livro.getAutoria(),
+                livro.getEditora(), livro.getCategoria().toString(), livro.getPrecoVenda());
+    }
+}
