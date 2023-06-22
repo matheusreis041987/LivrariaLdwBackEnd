ca9d65c6d522a8c895173752ddf4b1249523d8b4
vsantsal <vsantsal@gmail.com>
2023-06-22 14:46:44 -0300

    impl basica de atualizar_preco_de_venda

--- src/main/java/br/pucrioaps/livrarialdw/service/LivroService.java
+++ src/main/java/br/pucrioaps/livrarialdw/service/LivroService.java
@@ -3,6 +3,7 @@ package br.pucrioaps.livrarialdw.service;
 import br.pucrioaps.livrarialdw.dto.CabecalhoLivroDTO;
 import br.pucrioaps.livrarialdw.dto.CadastroDeLivroDTO;
 import br.pucrioaps.livrarialdw.dto.DetalheDeLivroDTO;
+import br.pucrioaps.livrarialdw.dto.PrecoVendaLivroDTO;
 import br.pucrioaps.livrarialdw.model.entity.Livro;
 import br.pucrioaps.livrarialdw.model.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +31,12 @@ public class LivroService {
         Livro livroASalvar = dados.toLivro();
         Livro livroSalvo = this.repository.save(livroASalvar);
         return new DetalheDeLivroDTO(livroSalvo);
+    }
+
+    public CabecalhoLivroDTO atualizar(PrecoVendaLivroDTO dto) {
+        Livro livroAAtualizar = repository.getReferenceById(dto.id());
+        livroAAtualizar.setPrecoVenda(dto.precoVenda());
+        repository.save(livroAAtualizar);
+        return new CabecalhoLivroDTO(livroAAtualizar);
     }
 }
