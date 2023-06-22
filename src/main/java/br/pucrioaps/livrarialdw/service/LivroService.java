935ddd0bb0890ff4e3f979aa8189f950aefec545
vsantsal <vsantsal@gmail.com>
2023-06-21 21:27:44 -0300

    endpoint de detalhes de livro funcional para id valido

---
+++ src/main/java/br/pucrioaps/livrarialdw/service/LivroService.java
@@ -0,0 +1,9 @@
+package br.pucrioaps.livrarialdw.service;
+
+import br.pucrioaps.livrarialdw.dto.DetalheDeLivroDTO;
+
+public class LivroService {
+    public DetalheDeLivroDTO detalhar(Long id) {
+        return null;
+    }
+}
