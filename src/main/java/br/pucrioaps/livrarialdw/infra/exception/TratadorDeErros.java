babd8ec57b1a3f9bd03a801e27d440d54845517a
vsantsal <vsantsal@gmail.com>
2023-06-21 21:53:36 -0300

    teste para endpoint de detalhamento de livro com id invalido

---
+++ src/main/java/br/pucrioaps/livrarialdw/infra/exception/TratadorDeErros.java
@@ -0,0 +1,15 @@
+package br.pucrioaps.livrarialdw.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Livro não encontrado");
+    }
+}
--- src/main/java/br/pucrioaps/livrarialdw/service/LivroService.java
+++ src/main/java/br/pucrioaps/livrarialdw/service/LivroService.java
@@ -1,9 +1,17 @@
 package br.pucrioaps.livrarialdw.service;
 
 import br.pucrioaps.livrarialdw.dto.DetalheDeLivroDTO;
+import br.pucrioaps.livrarialdw.model.entity.Livro;
+import br.pucrioaps.livrarialdw.model.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LivroService {
+    @Autowired
+    private LivroRepository repository;
     public DetalheDeLivroDTO detalhar(Long id) {
-        return null;
+        Livro livro = repository.getReferenceById(id);
+        return new DetalheDeLivroDTO(livro);
     }
 }
