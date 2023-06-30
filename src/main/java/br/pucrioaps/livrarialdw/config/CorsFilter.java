84b0cce4a358dcef02f8e750f18c98563ae8d1e1
vsantsal <vsantsal@gmail.com>
2023-06-30 20:57:02 -0300

    feat: adicionando configuracao de cors filter

---
+++ src/main/java/br/pucrioaps/livrarialdw/config/CorsFilter.java
@@ -0,0 +1,35 @@
+package br.pucrioaps.livrarialdw.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
